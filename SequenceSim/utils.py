e6e715a71e3c8ae00553243bdd38c640f83b2c84
osedo <osedo@tcd.ie>
2025-01-27 17:41:19 +0000

    Multiple motifs at percentages

--- SequenceSim/utils.py
+++ SequenceSim/utils.py
@@ -32,32 +32,26 @@ def motifer(mfile):
     return tfbs
 
 
-
-
-def markov_simulations(seq_parameters, n):
+def markov_simulations_motif(seq_parameters, n, files, indexes, presence):
     """Repeat simulations n times""" 
-    seqs = [markov_seq_string(init_freq = seq_parameters['init_freq'], \
+    seqs = numpy.array([markov_seq_string(init_freq = seq_parameters['init_freq'], \
                                transition_dic = seq_parameters['transition_dic'], \
                                transition_states = seq_parameters['transition_states'], \
                                n = seq_parameters['n'], \
-                               order = seq_parameters['order']) for i in range(n)]
-    
-    seqs_encoded = numpy.array([one_hot_encode(seq_i, seq_parameters['transition_states']) for seq_i in seqs])
-    return seqs_encoded
-
-def markov_simulations_motif(seq_parameters, n, file, index):
-    """Repeat simulations n times""" 
-    seqs = [markov_seq_string(init_freq = seq_parameters['init_freq'], \
-                               transition_dic = seq_parameters['transition_dic'], \
-                               transition_states = seq_parameters['transition_states'], \
-                               n = seq_parameters['n'], \
-                               order = seq_parameters['order']) for i in range(n)]
-    
-    if file is not None:
-        mots = [motifer(file) for i in range(n)]
-        start = numpy.random.randint(low=index[0], high=index[1], size = n)
-        for i, s in enumerate(seqs):
-            seqs[i] = s[:start[i]] + mots[i] + s[start[i]+len(mots[i]):]
+                               order = seq_parameters['order']) for i in range(n)])
+    # add motifs
+    if files is not None:
+        for file, index, p in zip(files, indexes, presence):
+            if file is not None:
+                
+                m = numpy.random.choice(range(n), size= round(n*p), replace=False)
+                
+                mots = [motifer(file) for i in m]
+                start = numpy.random.randint(low=index[0], high=index[1], size = len(m))
+                
+                for t, i in enumerate(m):
+                    seqs[i] = seqs[i][:start[t]] + mots[t] + seqs[i][start[t]+len(mots[t]):]
+                
 
     seqs_encoded = numpy.array([one_hot_encode(seq_i, seq_parameters['transition_states']) for seq_i in seqs])
     return seqs_encoded
@@ -65,13 +59,15 @@ def markov_simulations_motif(seq_parameters, n, file, index):
 
 def experiments_markov_binary(cases_seq_parameters, controls_seq_parameters, \
                               cases_n, controls_n, \
-                              cases_motif_file = None, cases_index = None, \
-                              controls_motif_file = None, controls_index = None):
+                              cases_motif_file = None, cases_index = None, cases_presence = None, \
+                              controls_motif_file = None, controls_index = None, controls_presence = None):
     """Allow different simulations for cases and controls allowing different parameters for cases and controls"""
-    X_cases = markov_simulations_motif(cases_seq_parameters, cases_n, cases_motif_file, cases_index)
+    X_cases = markov_simulations_motif(cases_seq_parameters, cases_n, \
+                                       cases_motif_file, cases_index, cases_presence)
     y_cases = numpy.repeat(1, cases_n)
     
-    X_controls = markov_simulations_motif(controls_seq_parameters, controls_n, controls_motif_file, controls_index)
+    X_controls = markov_simulations_motif(controls_seq_parameters, controls_n, \
+                                          controls_motif_file, controls_index, controls_presence)
     y_controls = numpy.repeat(0, controls_n).reshape(controls_n, 1)
     
     X = numpy.vstack([X_cases, X_controls])
